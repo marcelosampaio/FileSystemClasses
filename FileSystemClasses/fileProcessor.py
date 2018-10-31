5ffa7e70d5be1eee3e5f2ee9aa15b127483654fd
Marcelo <marcelo@marcelo.local>
2018-10-31 16:43:00 -0300

    Initial commit

---
+++ FileSystemClasses/fileProcessor.py
@@ -0,0 +1,47 @@
+# FileProcessor.py
+# this a sample program that will use module FileSystemClasses.py for iterating with files
+
+## Modules
+import fileSystemClasses
+
+# -------------------
+# Function Definition
+# -------------------
+def createFile(filePath):
+	# file system classes - file manager object - create a new file
+	fsc = fileSystemClasses.FileManager()
+	# set file path
+	fsc.setFilePath(filePath)
+	# create file for writing
+	fsc.createFile()
+
+def appendFile(filePath, fileContent):
+	# file system classes - file manager object - create a new file
+	fsc = fileSystemClasses.FileManager()
+	# set file path
+	fsc.setFilePath(filePath)
+	# create file for writing
+	fsc.appendFile(fileContent)
+
+
+# -------------------
+# Main Body
+# -------------------
+
+fileNamePath = "/Users/marcelo/Documents/MAS/Python_Production/FileSystemClasses/outputFile.txt"
+
+# ----------------------
+# crete the working file
+# ----------------------
+createFile(fileNamePath)
+
+
+# -----------------------------
+# append some content to a file
+# -----------------------------
+content = "Second line has been appended\n"
+appendFile(fileNamePath, content)
+
+
+
+
---
+++ FileSystemClasses/fileSystemClasses.py
@@ -0,0 +1,37 @@
+# libraries
+import os
+
+# FileSystemClasses.py
+class FileManager(object):
+	def __init__(self):
+		print("🍺 File Manager created.")
+
+	def setFilePath(self, filePath="default_value"):
+		self.filePath = filePath
+
+		print("🍺 This is the file path ", filePath)
+
+
+	def createFile(self):
+		instanceFile = open(self.filePath, "w")
+		instanceFile.write("")
+		instanceFile.close
+
+		print("🍺 -FileManager-createFileForOperation finished")
+		print("🍺 -...-FilePath: ", self.filePath)
+		print("🍺 -FileManager-end-method")
+
+	def appendFile(self, content):
+		self.content = content
+		instanceFile = open(self.filePath, "a")
+		instanceFile.write(content)
+		instanceFile.close		
+
+
+		print("🍺 -FileManager-appendFile finished")
+		print("🍺 -...-FilePath: ", self.filePath)
+		print("🍺 -...-Content: ", content)
+		print("🍺 -FileManager-end-method")
+
+
+
